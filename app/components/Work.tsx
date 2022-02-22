44bd1d62fe337fae6e015a1c1ccfeac3e3ebeaf0
Joseph Khawly <jkhawly@mouthwateringmedia.com>
2022-02-21 19:49:13 -0700

    new work card layout

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -1,4 +1,25 @@
-export default function Work() {
+interface Project {
+    name: string;
+    slug: string;
+    image: [{
+        url: string;
+    }];
+}
+
+function ProjectCard({ image, name, slug }: { image: string, name: string, slug: string }) {
+    return (
+        <div className="border mockup-window bg-base-300 h-36 lg:h-72">
+            <p className="absolute top-3.5 right-4">{name}</p>
+            <img
+                src={image}
+                className="w-full h-full object-cover"
+            />
+        </div>
+    )
+}
+
+
+export default function Work({ projects }: { projects: Project[] }) {
     return (
         <div className="block-container">
             <section className="w-full">
@@ -6,15 +27,9 @@ export default function Work() {
                 <p className="section-paragraph">I’ve had the pleasure of working with multiple Fortune 500 companies, designing and implementing both frontend and backend.</p>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-                    <img src="https://images.unsplash.com/photo-1576153192396-180ecef2a715?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" className="w-full  h-36 lg:h-72 object-cover" />
-                    <img src="https://images.unsplash.com/photo-1545235617-9465d2a55698?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" className="w-full  h-36 lg:h-72 object-cover" />
-                    <img src="https://images.unsplash.com/photo-1586717791821-3f44a563fa4c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80" className="w-full hidden md:block  h-36 lg:h-72 object-cover" />
-                    <img src="https://images.unsplash.com/photo-1558655146-d09347e92766?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80" className="w-full hidden md:block  h-36 lg:h-72 object-cover" />
-                    <img src="https://images.unsplash.com/photo-1547119957-637f8679db1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" className="w-full hidden md:block  h-36 lg:h-72 object-cover" />
-                    <img src="https://images.unsplash.com/photo-1559028012-481c04fa702d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1336&q=80" className="w-full hidden md:block  h-36 lg:h-72 object-cover" />
-                    <img src="https://images.unsplash.com/photo-1603969072881-b0fc7f3d77d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80s" className="w-full hidden md:block  h-36 lg:h-72 object-cover" />
-                    <img src="https://images.unsplash.com/photo-1618761714954-0b8cd0026356?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" className="w-full hidden md:block  h-36 lg:h-72 object-cover" />
-                    <img src="https://images.unsplash.com/photo-1545235617-7a424c1a60cc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80" className="w-full hidden md:block md:col-span-2 lg:col-span-1  h-36 lg:h-72 object-cover" />
+                    {projects.map(project => (
+                        <ProjectCard key={project.slug} name={project.name} image={project.image[0].url} />
+                    ))}
                 </div>
             </section>
         </div>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,39 @@
-import Clients from "~/components/Clients";
-import Hero from "~/components/Hero";
-import Hire from "~/components/Hire";
+// import Clients from "~/components/Clients";
+// import Hero from "~/components/Hero";
+// import Hire from "~/components/Hire";
 import Work from "~/components/Work";
+import { GraphQLClient, gql } from 'graphql-request';
+import { json, LoaderFunction, useLoaderData } from "remix";
+
+const queryProjects = gql`
+    {
+        projects {
+          name
+          slug
+          image {
+            url
+          }
+        }
+    }
+`
+
+export let loader: LoaderFunction = async () => {
+  const graphcms = new GraphQLClient(
+    'https://api-us-west-2.graphcms.com/v2/ckzxewqxy4l2h01ys17lt7m2b/master'
+  );
+
+  const { projects } = await graphcms.request(queryProjects);
+  return json({ projects });
+}
 
 export default function Index() {
+  let data = useLoaderData()
   return (
     <>
-      <Hero />
-      <Work />
-      <Clients />
-      <Hire />
+      {/* <Hero /> */}
+      <Work projects={data.projects} />
+      {/* <Clients /> */}
+      {/* <Hire /> */}
     </>
   );
 }
