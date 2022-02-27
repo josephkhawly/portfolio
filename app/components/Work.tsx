38fd04b7a7209893799c7d26916a25b8305780c5
Joseph Khawly <jkhawly@mouthwateringmedia.com>
2022-02-26 17:17:35 -0700

    project detail page grid layout

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -1,7 +1,7 @@
 import { useWindowSize } from 'react-use';
 import { Link } from 'remix';
 
-interface Project {
+type Project = {
     name: string;
     slug: string;
     image: [{
@@ -10,32 +10,32 @@ interface Project {
 }
 
 type CardProps = {
-    image: string;
-    name: string;
-    slug: string;
+    project: Project;
 }
 
-function ProjectCard({ image, name, slug }: CardProps) {
+function ProjectCard({ project }: CardProps) {
     return (
-        <Link prefetch='intent' to={`/projects/${slug}`} >
+        <Link prefetch='intent' to={`/projects/${project.slug}`}>
             <div className="group border mockup-window bg-base-300 lg:h-72 hover:bg-[#0025FF] cursor-pointer transition-colors shadow-xl">
-                <p className="absolute top-3.5 right-4 font-mono opacity-0 group-hover:opacity-100 transition-opacity">{name}</p>
-                <img src={image} className="w-full h-full object-cover" />
+                <p className="absolute top-3.5 right-4 font-mono opacity-0 group-hover:opacity-100 transition-opacity">{project.name}</p>
+                <img src={project.image[0].url} className="w-full h-full object-cover" />
             </div>
         </Link>
     )
 }
 
-function MobileProjectCard({ image, name, slug }: CardProps) {
+function MobileProjectCard({ project }: CardProps) {
     return (
-        <div className="mockup-phone md:w-auto shadow-xl">
-            <div className="camera"></div>
-            <div className="display">
-                <div className="artboard artboard-demo phone-1">
-                    <img src={image} className="w-full h-full object-cover" />
+        <Link prefetch='intent' to={`/projects/${project.slug}`}>
+            <div className="mockup-phone md:w-auto shadow-xl">
+                <div className="camera"></div>
+                <div className="display">
+                    <div className="artboard artboard-demo phone-1">
+                        <img src={project.image[0].url} className="w-full h-full object-cover" />
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
@@ -47,7 +47,7 @@ export default function Work({ projects }: { projects: Project[] }) {
             <section className="w-full">
                 <h2 className="secondary-title">Projects I've worked on</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-                    {projects.map(project => <ProjectCard key={project.slug} name={project.name} image={project.image[0].url} slug={project.slug} />)}
+                    {projects.map(project => <ProjectCard key={project.slug} project={project} />)}
                 </div>
             </section>
         </div>
--- app/routes/projects/$slug.tsx
+++ app/routes/projects/$slug.tsx
@@ -25,13 +25,15 @@ export const loader: LoaderFunction = async ({ params }) => {
 export default function ProjectSlug() {
     let data = useLoaderData()
     return (
-        <div className="block-container">
-            <div className="border mockup-window bg-base-300 cursor-pointer shadow-xl">
+        <div className="block-container grid lg:grid-cols-2 gap-10">
+            <div className="border mockup-window bg-base-300 shadow-xl">
                 <img src={data.project.image[0].url} className="w-full h-full object-cover" />
             </div>
-            <h2 className="secondary-title">{data.project.name}</h2>
-            <div className="prose">
-                {data.project.description}
+            <div>
+                <h2 className="secondary-title">{data.project.name}</h2>
+                <div className="prose">
+                    {data.project.description}
+                </div>
             </div>
         </div>
     )
