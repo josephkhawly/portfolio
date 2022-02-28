e9305e0d2a0c842bb80f696598e6fea4f21bd71d
Joseph Khawly <jkhawly@mouthwateringmedia.com>
2022-02-27 18:27:20 -0700

    Project card tweaks

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -1,6 +1,5 @@
-import { useWindowSize } from 'react-use';
-import { Link } from 'remix';
-import { ImageType, Img } from './Img';
+import { Link } from 'remix'
+import { ImageType, Img } from './Img'
 
 type Project = {
     name: string;
@@ -15,7 +14,7 @@ type CardProps = {
 function ProjectCard({ project }: CardProps) {
     return (
         <Link prefetch='intent' to={`/projects/${project.slug}`}>
-            <div className="group border mockup-window bg-base-300 lg:h-72 hover:bg-[#0025FF] cursor-pointer transition-colors shadow-xl">
+            <div className="group border mockup-window bg-base-300">
                 <p className="absolute top-3.5 right-4 font-mono opacity-0 group-hover:opacity-100 transition-opacity">{project.name}</p>
                 <Img image={project.image[0]} />
             </div>
@@ -26,10 +25,10 @@ function ProjectCard({ project }: CardProps) {
 function MobileProjectCard({ project }: CardProps) {
     return (
         <Link prefetch='intent' to={`/projects/${project.slug}`}>
-            <div className="mockup-phone md:w-auto shadow-xl">
+            <div className="mockup-phone shadow-xl block">
                 <div className="camera"></div>
                 <div className="display">
-                    <div className="artboard artboard-demo phone-1">
+                    <div className="artboard phone-1">
                         <Img image={project.image[0]} />
                     </div>
                 </div>
@@ -40,13 +39,12 @@ function MobileProjectCard({ project }: CardProps) {
 
 
 export default function Work({ projects }: { projects: Project[] }) {
-    const { width } = useWindowSize();
     return (
         <div className="block-container">
             <section className="w-full">
                 <h2 className="secondary-title">Projects I've worked on</h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-                    {projects.map(project => width > 768 ? <ProjectCard key={project.slug} project={project} /> : <MobileProjectCard key={project.slug} project={project} />)}
+                <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 mt-6">
+                    {projects.map(project => <ProjectCard key={project.slug} project={project} />)}
                 </div>
             </section>
         </div>
